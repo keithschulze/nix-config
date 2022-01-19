ac6be25a58ed48cb6beba89d3ccbb1bf905b4b0c
Keith Schulze <keith.schulze@hey.com>
2022-01-19 16:19:26 +1100

    WIP i3 config

---
+++ modules/desktop/i3/i3wm.nix
@@ -0,0 +1,146 @@
+{ config, lib, pkgs, nix-colors, ... }:
+let
+  term = "WINIT_X11_SCALE_FACTOR=1.0 alacritty";
+
+  mod = "Mod1";
+  exec = "exec --no-startup-id";
+
+  alwaysRun = [
+    "systemctl --user restart polybar"
+    "xset r rate 250 50"
+  ];
+
+  run = [
+    "i3-msg workspace number 1"
+  ];
+in
+{
+  colorscheme = (import "${nix-colors}/schemes").tokyonight;
+
+  xsession = {
+    enable = true;
+    windowManager.i3 = {
+      enable = true;
+      package = pkgs.i3-gaps;
+      extraPackages = with pkgs; [
+        i3status # gives you the default i3 status bar
+        i3lock-fancy # default i3 screen locker
+        i3blocks # if you are planning on using i3blocks over i3status
+        polybar
+        xclip
+      ];
+      config = {
+        bars = [ ];
+        gaps = {
+          inner = 5;
+        };
+        window.border = 5;
+        floating.border = 5;
+        modifier = "${mod}";
+        terminal = "${term}";
+        keybindings = lib.mkOptionDefault {
+          "${mod}+Return" = "${exec} ${term}";
+        };
+        colors = with config.colorscheme; {
+          focused = {
+            border = "#${colors.base07}";
+            childBorder = "#${colors.base07}";
+            background = "#${colors.base07}";
+            text = "#${colors.base07}";
+            indicator = "#${colors.base07}";
+          };
+          focusedInactive = {
+            border = "#${colors.base03}";
+            childBorder = "#${colors.base03}";
+            background = "#${colors.base03}";
+            text = "#${colors.base03}";
+            indicator = "#${colors.base03}";
+          };
+          unfocused = {
+            border = "#${colors.base03}";
+            childBorder = "#${colors.base03}";
+            background = "#${colors.base03}";
+            text = "#${colors.base03}";
+            indicator = "#${colors.base03}";
+          };
+          urgent = {
+            border = "#${colors.base03}";
+            childBorder = "#${colors.base03}";
+            background = "#${colors.base00}";
+            text = "#${colors.base05}";
+            indicator = "#${colors.base00}";
+          };
+        };
+        startup = []
+        ++
+        builtins.map ( command:
+            {
+              command = command;
+              always = true;
+              notification = false;
+            }
+          ) alwaysRun
+        ++
+          builtins.map ( command:
+            {
+              command = command;
+              notification = false;
+            }
+          ) run;
+      };
+    };
+  };
+
+  services.polybar = {
+    enable = true;
+    package = pkgs.polybarFull;
+    config = with config.colorscheme; {
+      "bar/main" = {
+        background = "#${colors.base00}";
+        foreground = "#${colors.base05}";
+        modules-left = "i3";
+        modules-center = "mpd";
+        modules-right = "battery date";
+        border-left-size = 1;
+        border-left-color = "#${colors.base00}";
+        border-right-size = 1;
+        border-right-color = "#${colors.base00}";
+        border-top-size = 2;
+        border-top-color = "#${colors.base00}";
+        border-bottom-size = 2;
+        border-bottom-color = "#${colors.base00}";
+        font-0 = "lemon:pixelsize=10;1";
+      };
+      "module/battery" = {
+        type = "internal/battery";
+      };
+      "module/date" =
+        let
+          calnotify = pkgs.writeShellScript "calnotify.sh" ''
+            day="$(${pkgs.coreutils}/bin/date +'%-d ' | ${pkgs.gnused}/bin/sed 's/\b[0-9]\b/ &/g')"
+            cal="$(${pkgs.utillinux}/bin/cal | ${pkgs.gnused}/bin/sed -e 's/^/ /g' -e 's/$/ /g' -e "s/$day/\<span color=\'#${base0B-hex}\'\>\<b\>$day\<\/b\>\<\/span\>/" -e '1d')"
+            top="$(${pkgs.utillinux}/bin/cal | ${pkgs.gnused}/bin/sed '1!d')"
+            ${pkgs.libnotify}/bin/notify-send "$top" "$cal"
+          '';
+        in
+        {
+          type = "internal/date";
+          date = "%I:%M %p    %a %b %d";
+          label = "%{A1:${calnotify}:}%date%%{A}";
+          format = "<label>";
+          label-padding = 5;
+        };
+      "module/i3" = {
+        type = "internal/i3";
+        label-unfocused-foreground = "#${base03}";
+        label-urgent-foreground = "#${base09}";
+        label-unfocused-padding = 1;
+        label-focused-padding = 1;
+        label-urgent-padding = 1;
+      };
+    };
+    script = ''
+      polybar main &
+    '';
+  };
+}
