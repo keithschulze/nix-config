b53c2d66322d00a63911384ebb9a892f0bfad6bf
Keith Schulze <keith.schulze@hey.com>
2022-01-26 21:41:56 +1100

    Add personal-vm role

---
+++ users/keithschulze/nix-home/role/personal-vm/default.nix
@@ -0,0 +1,120 @@
+{ config, pkgs, lib, ... }:
+
+{
+
+  home.packages = with pkgs; [
+    # utils
+    jq
+    htop
+    fzf
+    ripgrep
+    fd
+
+    # dev
+    shellcheck
+    tmux
+    tmuxinator
+    graphviz
+
+    # languages
+    nodejs
+
+    # tools
+    poetry
+    cookiecutter
+  ];
+
+  programs.ssh = {
+    enable = true;
+    matchBlocks = {
+      nix-aws = {
+        hostname = "13.239.106.150";
+        user = "root";
+        extraOptions = {
+          AddKeysToAgent = "yes";
+        };
+        identityFile = "~/.ssh/id_rsa";
+        localForwards = [
+          {
+            bind.port = 8888;
+            host.address = "127.0.0.1";
+            host.port = 8888;
+          }
+        ];
+      };
+    };
+  };
+
+  programs.gpg = {
+    enable = true;
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "Keith Schulze";
+    userEmail = "keith.schulze@hey.com";
+    aliases = {
+      co = "checkout";
+      up = "!git pull --rebase --prune $@";
+      cob = "checkout -b";
+      cm = "!git add -a && git commit -m";
+      save = "!git add -a && git commit -m 'savepoint'";
+      wip = "!git add -u && git commit -m 'wip'";
+      undo = "reset head~1 --mixed";
+      amend = "commit -a --amend";
+      wipe = "!git add -a && git commit -qm 'wipe savepoint' && git reset head~1 --hard";
+    };
+    extraConfig = {
+      github.user = "keithschulze";
+      color.ui = true;
+      push.default = "simple";
+      pull.rebase = true;
+      merge.conflictstyle = "diff3";
+      credential.helper = "osxkeychain";
+      diff.tool = "vimdiff";
+      difftool.prompt = false;
+      core.commitGraph = true;
+      gc.writeCommitGraph = true;
+      init.defaultBranch = "main";
+    };
+  };
+
+  programs.neovim = (import ../../program/neovim/default.nix) { inherit config; inherit pkgs; inherit lib; };
+
+  programs.tmux = (import ../../program/tmux/default.nix) { inherit pkgs; };
+
+  programs.starship = import ../../program/starship/default.nix;
+
+  programs.zsh = lib.attrsets.recursiveUpdate (import ../../program/zsh/default.nix) {
+    initExtra = ''
+      if [ -n "''\${commands[fzf-share]}" ]; then
+        source "$(fzf-share)/key-bindings.zsh"
+        source "$(fzf-share)/completion.zsh"
+      fi
+    '';
+    oh-my-zsh = {
+      enable = true;
+      plugins = [
+        "git"
+        "docker-compose"
+        "gcloud"
+        "ripgrep"
+        "terraform"
+        "tmux"
+        "kubectl"
+      ];
+    };
+  };
+
+  programs.alacritty = {
+    enable = true;
+    settings = import ../../program/alacritty/default-settings.nix;
+  };
+
+  programs.direnv = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  home.file.".background-image".source = ./wallpaper.jpg;
+}
