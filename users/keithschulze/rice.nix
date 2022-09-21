f81ccea3c16473b84f6a5fc8cbf0a84d50007dcc
Keith Schulze <keith.schulze@hey.com>
2022-09-21 21:12:27 +1200

    Remove unused wallpaper settings from rice

--- users/keithschulze/rice.nix
+++ users/keithschulze/rice.nix
@@ -4,12 +4,9 @@ with inputs.nix-colors.lib { inherit pkgs; };
 
 let
   currentScheme.parallels-vm = "tokyonight";
-  currentWallpaper.parallels-vm = "mountain-yosemite-purple";
-  currentMode = null;
 in
 {
   imports = [ inputs.nix-colors.homeManagerModule ];
-  # home.packages = with pkgs; [ setscheme setwallpaper ];
 
   colorscheme =
     if currentScheme.${hostname} != null then
@@ -18,16 +15,5 @@ in
       colorschemeFromPicture {
         path = config.wallpaper;
         kind = currentMode;
-      };
-
-  wallpaper =
-    if currentWallpaper.${hostname} != null then
-      pkgs.wallpapers.${currentWallpaper.${hostname}}
-    else
-      nixWallpaperFromScheme {
-        scheme = config.colorscheme;
-        width = 2560;
-        height = 1440;
-        logoScale = 4.5;
       };
 }
