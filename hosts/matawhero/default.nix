340033ff54eff928a1b3d0e07bea74a89b4a8e73
Keith Schulze <keith.schulze@hey.com>
2022-09-27 20:27:48 +1300

    Tidy up matawhero darwin host config

--- hosts/matawhero/default.nix
+++ hosts/matawhero/default.nix
@@ -29,23 +29,9 @@ in {
     home = "/Users/keithschulze";
   };
 
-  services.nix-daemon.enable = true;
-
-  launchd.user.agents = {
-    "lorri" = {
-      serviceConfig = {
-        WorkingDirectory = (builtins.getEnv "HOME");
-        EnvironmentVariables = { };
-        KeepAlive = true;
-        RunAtLoad = true;
-        StandardOutPath = "/var/tmp/lorri.log";
-        StandardErrorPath = "/var/tmp/lorri.log";
-      };
-      script = ''
-        source ${config.system.build.setEnvironment}
-        exec ${lorri}/bin/lorri daemon
-      '';
-    };
+  services = {
+    nix-daemon.enable = true;
+    lorri.enable = true;
   };
 
   nix.extraOptions = ''
