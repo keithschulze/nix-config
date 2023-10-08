2693b274be03ff2946d924c7c013068ef14088dc
Keith Schulze <kschulze@seek.co.nz>
2023-10-09 09:43:45 +1300

    Tweak tmux status bar layout

--- home/program/tmux/default.nix
+++ home/program/tmux/default.nix
@@ -126,9 +126,23 @@
     bind-key 0 run "tmux split-window -l 12 'bash -ci ftpane'"
 
     # Theme
-    set -g @catppuccin_flavour 'frappe'
-    set -g @catppuccin_date_time "%Y-%m-%d %H:%M"
-    set -g @catppuccin_window_tabs_enabled on
+    set -g @catppuccin_window_right_separator "█ "
+    set -g @catppuccin_window_number_position "right"
+    set -g @catppuccin_window_middle_separator " | "
+
+    set -g @catppuccin_window_default_fill "none"
+    set -g @catppuccin_window_default_text "#W"
+
+    set -g @catppuccin_window_current_fill "all"
+    set -g @catppuccin_window_current_text "#W"
+
+    set -g @catppuccin_status_modules_right "application session date_time"
+    set -g @catppuccin_status_left_separator  ""
+    set -g @catppuccin_status_right_separator " "
+    set -g @catppuccin_status_right_separator_inverse "yes"
+    set -g @catppuccin_status_fill "all"
+    set -g @catppuccin_status_connect_separator "no"
+    set -g @catppuccin_date_time_text "%Y-%m-%d %H:%M:%S"
   '';
   plugins = with pkgs.tmuxPlugins; [
     catppuccin
