30bf4b36b487dcf6f4e3cfa3e2e094fcd25241a8
Keith Schulze <keith@schulze.co.nz>
2025-01-23 21:57:35 +1300

    Catppuccin for vscode

--- home/program/vscode/default.nix
+++ home/program/vscode/default.nix
@@ -6,15 +6,15 @@
   enableExtensionUpdateCheck = false;
   mutableExtensionsDir = false;
   extensions = with pkgs; [
-    vscode-extensions.bbenoist.nix
     vscode-extensions.arrterian.nix-env-selector
+    vscode-extensions.bbenoist.nix
+    vscode-extensions.catppuccin.catppuccin-vsc
+    vscode-extensions.github.copilot
+    vscode-extensions.github.copilot-chat
+    vscode-extensions.jebbs.plantuml
     vscode-extensions.ms-azuretools.vscode-docker
     vscode-extensions.redhat.vscode-yaml
     vscode-extensions.vscodevim.vim
-    vscode-extensions.github.copilot
-    vscode-extensions.github.copilot-chat
-    vscode-extensions.github.github-vscode-theme
-    vscode-extensions.jebbs.plantuml
   ] ++ extraExtensions;
 
   userSettings = builtins.fromJSON(builtins.readFile ./settings.json) // extraUserSettings;
