473605786cbdbc05d22ad6460bd624220a85f721
Keith Schulze <kschulze@seek.co.nz>
2025-02-26 10:19:32 +1300

    Add keybinding to launch new brower window

--- users/kschulze/default.nix
+++ users/kschulze/default.nix
@@ -144,6 +144,7 @@ in {
         alt-shift-tab = "move-workspace-to-monitor --wrap-around next";
 
         alt-shift-semicolon = "mode service";
+        alt-b = "exec-and-forget open -n -a 'Google Chrome' --args '--new-window'";
       };
 
       mode.service.binding = {
