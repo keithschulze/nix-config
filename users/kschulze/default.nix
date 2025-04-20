065ffc25c717c218bcf844aa88b162d55a1bbb15
Keith Schulze <keith@schulze.co.nz>
2025-04-20 13:40:16 +1200

    Configure avant to use claude

--- users/kschulze/default.nix
+++ users/kschulze/default.nix
@@ -15,7 +15,12 @@ let
       config = ''
         require("avante_lib").load()
         require("avante").setup({
-          provider = "copilot"
+          provider = "copilot",
+          copilot = {
+            model = "claude-3.7-sonnet",
+            temperature = 0,
+            max_tokens = 8192,
+          }
         })
       '';
     }
